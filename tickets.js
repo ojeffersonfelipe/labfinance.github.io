b60f94fcdf2391309a139157006df72bd47a9da3
Jéfferson Felipe <92769670+ojeffersonfelipe@users.noreply.github.com>
2023-12-31 00:47:41 -0300

    Initial commit

---
+++ tickets.js
@@ -0,0 +1,27 @@
+const Tickets = [
+    {
+        clientName: 'Xp Investimentos',
+        ticketNumber: '85743',
+        withPerson: 'Daniela Cristina',
+        status: 'Recebido'
+    },
+    {
+        clientName: 'Santander',
+        ticketNumber: '97245',
+        withPerson: 'Stefany Tinti',
+        status: 'Aberto'
+    },
+    {
+        clientName: 'Bradesco',
+        ticketNumber: '36452',
+        withPerson: 'Jéfferson Felipe',
+        status: 'Atrasado'
+    },
+    {
+        clientName: 'Azimut Brasil',
+        ticketNumber: '36455',
+        withPerson: 'Jéfferson Felipe',
+        status: 'Aberto'
+    },
+];
+
