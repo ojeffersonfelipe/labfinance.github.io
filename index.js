0200f015f0ffcf4c492888c76b6b235b07ecc517
Jéfferson Felipe <92769670+ojeffersonfelipe@users.noreply.github.com>
2024-01-16 00:15:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-document.getElementById('loginForm').onsubmit = async function(event) {
+const sideMenu = document.querySelector('aside');
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+
+const darkMode = document.querySelector('.dark-mode');
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+});
+
+darkMode.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode-variables');
+    darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
+    darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
+})
+
+document.getElementById('loginForm').onsubmit = async function (event) {
     event.preventDefault(); // Previne o comportamento padrão do formulário
 
     let email = document.getElementById('email').value;
