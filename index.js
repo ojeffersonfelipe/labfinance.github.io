d5efa44144ced3e14712e0618f60ef8d131ea34a
Jéfferson Felipe <92769670+ojeffersonfelipe@users.noreply.github.com>
2024-01-22 22:06:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,6 @@ const closeBtn = document.getElementById('close-btn');
 
 const darkMode = document.querySelector('.dark-mode');
 
-menuBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'block';
-});
-
-closeBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'none';
-});
-
 darkMode.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode-variables');
     darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
